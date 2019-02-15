2530d1b6130439f454b8781b8313fe0d788a1789
SyrymPP <syrymalisher@gmail.com>
2019-02-16 01:37:29 +0600

    new task

---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        private static void dublicate(string[] s, int n) // Method dublicate with 2 parameters
+        {
+            string[] ss = new string[n * 2]; // String array
+            for (int i = 0, j = 0; i < n; ++i)
+            {
+                ss[j++] = s[i]; // Doubling
+                ss[j++] = s[i];
+            }
+            for (int i = 0; i < 2 * n; ++i)
+                Console.Write(ss[i] + " ");
+        }
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine()); // reading first line and converting to int
+            int n = int.Parse(Console.ReadLine());
+            string[] numb = Console.ReadLine().Split(); // reading second line an array of string by split
+            dublicate(numb, n); // Method thats dubplicate each of element
+            Console.ReadKey();
+        }
+    }
+}
--- Week1/Task3/Task3/Properties/AssemblyInfo.cs
+++ Week1/Task3/Task3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("Task3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("Task3")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("55d057e0-ccb8-4bfc-bcf4-6e28604f2590")]
+[assembly: Guid("e2da67c8-c6bc-4a3d-b032-1b0723811bf6")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
