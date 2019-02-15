ee7705d6b65c0fc1b3939f352e39f5b4556e2c6a
SyrymPP <syrymalisher@gmail.com>
2019-02-16 04:33:31 +0600

    New changes

---
+++ Week2/Task3/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Task3
+{
+    class Program
+    {
+        static void rec(DirectoryInfo dir, string level)
+        {
+            Console.WriteLine(level + dir.Name);
+            level += "    ";
+            FileSystemInfo[] fs = dir.GetFileSystemInfos();
+            for (int i = 0; i < fs.Length; ++i)
+            {
+                if (fs[i].GetType() == typeof(FileInfo))
+                {
+                    Console.WriteLine(level + fs[i].Name);
+                }
+                else
+                {
+                    rec(fs[i] as DirectoryInfo, level);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Сырым\Desktop\Spring 2019");
+            rec(dir, "");
+            Console.ReadKey();
+        }
+    }
+}
